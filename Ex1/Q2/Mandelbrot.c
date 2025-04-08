46794d3412bd5fddfe6d17d0d95672e98de9abbb
ShaniGolomb <158572755+Shanig03@users.noreply.github.com>
2025-04-08 13:40:14 +0300

    Add files via upload

--- Ex1/Q2/Mandelbrot.c
+++ Ex1/Q2/Mandelbrot.c
@@ -8,11 +8,12 @@ void printUsage(const char* programName){
     fprintf(stderr, "Example: %s -0.5 0.5 1000\n", programName);
 }
 
+
 int isInMandelbrotSet(double complex c, int N, double M){
     double complex z = 0;
     for(int i = 0; i < N; ++i){
-        z = z * z + c;
-        if(cabs(z) > M){
+        z = z * z + c; // Calculate by the formula
+        if(cabs(z) > M){ // Calculate the absolute value of z
             return 0; // Not in the set
         }
     }
@@ -27,17 +28,17 @@ int main(int argc, char* argv[]) {
     }
 
     double real = atof(argv[1]);
-    double imagenary = atof(argv[2]);
+    double imaginary = atof(argv[2]);
     double M = 2.0;
 
     int N;
     if (argc >= 4) {
         N = atoi(argv[3]);
     } else {
-        N = 1000;
+        N = 1000; // Default value
     }
 
-    double complex c = real + imagenary * I;
+    double complex c = real + imaginary * I;
 
     if (isInMandelbrotSet(c, N, M)) {
         printf("The number is in the set\n");
